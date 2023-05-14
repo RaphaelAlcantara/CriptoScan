87757189ac842eef19a2781d290f89b5a09e74bf
RaphaelAlcantara <rvfra@discente.ifpe.edu.br>
2023-05-14 02:58:08 -0300

    Navbar - botões criados

---
+++ app/src/main/java/com/ifpe/criptoscan/NavBarActivity.java
@@ -0,0 +1,14 @@
+package com.ifpe.criptoscan;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class NavBarActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.nav_bar);
+    }
+}
