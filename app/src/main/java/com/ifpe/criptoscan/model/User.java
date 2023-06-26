d427ebbf67c41cc1d592c7dd91bb543e3b5c0f26
Gabriel Gouveia <gabriel.gouveia@live.com>
2023-06-26 00:25:34 -0300

    Ajuste da classe User, Getters e Setters

--- app/src/main/java/com/ifpe/criptoscan/model/User.java
+++ app/src/main/java/com/ifpe/criptoscan/model/User.java
@@ -2,6 +2,7 @@ package com.ifpe.criptoscan.model;
 
 import com.google.firebase.database.IgnoreExtraProperties;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @IgnoreExtraProperties
@@ -17,13 +18,39 @@ public class User {
     public User(String name, String email) {
         this.name = name;
         this.email = email;
+        this.favoritos = new ArrayList<>();
+        this.alertas = new ArrayList<>();
     }
 
     public String getName() {
         return name;
     }
 
+    public void setName(String name) {
+        this.name = name;
+    }
+
     public String getEmail() {
         return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public List<CriptoMoeda> getFavoritos() {
+        return favoritos;
+    }
+
+    public void setFavoritos(List<CriptoMoeda> favoritos) {
+        this.favoritos = favoritos;
+    }
+
+    public List<Alerta> getAlertas() {
+        return alertas;
+    }
+
+    public void setAlertas(List<Alerta> alertas) {
+        this.alertas = alertas;
     }
 }
